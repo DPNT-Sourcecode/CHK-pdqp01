1222a93ae72ffe21324d86ea32e938ded823b371
sbx_user1051 <sbx_user1051@169.254.195.205>
2019-05-12 18:56:07 +0000

    Sun May 12 18:56:07 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
+    if not skus.isalpha() and not skus.isupper():
+        return -1
+
     checkout_total = 0
     
     for sku in skus:
-        
+        if sku == "A":
+            checkout_total += 50
+        elif sku == "B":
+            checkout_total += 30
+        elif sku == "C":
+            checkout_total += 20
+        elif sku == "D":
+            checkout_total += 15
+
+    return checkout_total
+
 
 
