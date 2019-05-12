5bd315ddc77c6ae0f26217a74771067b37d3f023
sbx_user1051 <sbx_user1051@169.254.195.205>
2019-05-12 19:38:33 +0000

    Sun May 12 19:38:33 UTC 2019

---
+++ lib/solutions/CHK/checkout.py
@@ -0,0 +1,8 @@
+from typing import Dict
+
+class Checkout():
+    def __init__(self, price_list: Dict[str, int], offers: Offers);
+        self.price_list = price_list
+        self.offers = offers
+
+    
