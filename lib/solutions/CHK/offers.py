3220456ca770debe81235bb8d9662aa0cf599f39
sbx_user1051 <sbx_user1051@169.254.195.205>
2019-05-12 19:56:33 +0000

    Sun May 12 19:56:33 UTC 2019

--- lib/solutions/CHK/offers.py
+++ lib/solutions/CHK/offers.py
@@ -1,5 +1,20 @@
 from typing import Dict, Tuple
 
 class Offers:
-    def apply(sku_counts: Dict[str, int]) -> Tuple[int, Dict[str, int]]:
-        
+    MULTIBUYS = {
+        "A": {"number": 3, "cost": 130},
+        "B": {"number": 2, "cost": 45},
+    }
+
+    def apply(self, sku_counts: Dict[str, int]) -> Tuple[int, Dict[str, int]]:
+        offer_value = 0
+
+        for sku in sku_counts:
+            if sku in self.MULTIBUYS:
+                multibuy_count = sku_counts[sku] // self.MULTIBUYS[sku]["number"]
+                multibuy_cost = multibuy_count * self.MULTIBUYS[sku]["cost"]
+                offer_value += multibuy_cost
+
+                sku_counts[sku] = sku_counts[sku] % self.MULTIBUYS[sku]["number"]
+
+        return offer_value, sku_counts
